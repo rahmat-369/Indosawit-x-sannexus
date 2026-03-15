e854a90e4a86a746c96dfd759ed4c8c9c98ee741
R_hmt dev <rohmatulloh.3609@gmail.com>
2026-03-16 04:33:29 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Tambahan trigger untuk PWA (Otomatis dari vite-plugin-pwa)
+import { registerSW } from 'virtual:pwa-register'
+
+// Mengaktifkan Service Worker biar PWA jalan
+registerSW({ immediate: true })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-) 
+)
